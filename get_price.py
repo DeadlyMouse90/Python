459fb90a85841a515c2fbf3c197ab1d371de2a46
Ernane Domingues <ernanedomingues@outlook.com>
2021-06-29 21:39:30 -0300

    Update get_price.py

--- get_price.py
+++ get_price.py
@@ -2,10 +2,10 @@
 #using Yahoo Finance as the basis for price information
 
 from pandas_datareader import data as web
-import pandas as pd
 import matplotlib.pyplot as plt
+from IPython.display import display
 
-get_price=web.DataReader('KO', data_source='yahoo', start="01-01-2021",end="06-21-2021")
+get_price=web.DataReader('MSFT', data_source='yahoo', start="01-01-2021",end="06-29-2021")
 display(get_price)
 get_price["Adj Close"].plot(figsize=(15,10))
 plt.show()
